ac58f6c818a1bac62a90694ddd7bd3a7ece1e9b6
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-09 00:01:52 +0300

    protocol forms

---
+++ execom/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms_alchemy import model_form_factory
+
+
+from case.models import Case
+from .models import Protocol, Decision
+
+
+from app import db
+
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(self):
+        return db.session
+
+
+class ProtocolForm(ModelForm):
+    case = QuerySelectField(
+        "Дело",
+        query_factory=lambda: Case.query.all(),
+        allow_blank=True,
+    )
+    class Meta:
+        model = Protocol
+
+
+class DecisionForm(ModelForm):
+    case = QuerySelectField(
+        "Протокол",
+        query_factory=lambda: Protocol.query.all(),
+        allow_blank=True,
+    )
+    class Meta:
+        model = Decision
