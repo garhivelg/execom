044260af0b92b389a94928c84e1a0dc3f0c37b80
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-09 00:26:43 +0300

    Protocol form

--- execom/views.py
+++ execom/views.py
@@ -1,9 +1,13 @@
-from flask import render_template, redirect
+from flask import flash, render_template, redirect
 from flask.helpers import url_for
-from app import app
+from app import db, app
 
 
-from .models import Protocol
+from .models import Protocol, Decision
+from .forms import ProtocolForm
+
+
+from case.models import Case
 
 
 @app.route("/")
@@ -27,7 +31,31 @@ def list_protocols():
     )
 
 
-@app.route("/protocol/add")
+@app.route("/protocol/add", methods=["GET", "POST", ])
 @app.route("/protocol/edit/<int:protocol_id>", methods=["GET", "POST", ])
-def edit_protocol(protocol_id=None):
-    return redirect(url_for("list_cases"))
+@app.route("/protocol/add/<int:case_id>", methods=["GET", "POST", ])
+def edit_protocol(protocol_id=None, case_id=None):
+    if case_id is not None:
+        case = Case.query.get_or_404(case_id)
+        protocol = Protocol(case=case)
+    elif protocol_id is not None:
+        protocol = Protocol.query.get_or_404(protocol_id)
+    else:
+        protocol = Protocol()
+    form = ProtocolForm(obj=protocol)
+
+    if form.validate_on_submit():
+        form.populate_obj(protocol)
+        db.session.add(protocol)
+        if protocol.id:
+            flash("Протокол изменен")
+        else:
+            flash("Протокол добавлен")
+        db.session.commit()
+        return redirect(url_for("list_protocols"))
+
+    app.logger.debug(form.errors)
+
+    decisions = Decision.query.filter_by(protocol=protocol)
+
+    return render_template("edit_protocol.html", form=form, protocol=protocol, decisions=decisions)
