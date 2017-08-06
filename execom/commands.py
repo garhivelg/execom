42c084dfc32b390c1be15a38a5be843916fea70a
Dmitry Kutsenko <d2emonium@gmail.com>
2017-08-06 18:53:39 +0300

    Export to yaml

--- execom/commands.py
+++ execom/commands.py
@@ -1,16 +1,16 @@
 from app import manager, db
 from faker import Factory
 import random
+import yaml
 
 
-from .models import Protocol, Decision
+from .models import Protocol, Decision, Resolution
 from case.models import Register, Case
 
 
 @manager.command
 def fill():
     "Fill db with sample data"
-    print("hello")
     fake = Factory.create('ru_RU')
 
     registers = random.randint(10, 50)
@@ -57,3 +57,68 @@ def fill():
                     print("\t\t\tDecision#%d of %d: %s" % (decision_id, decisions, decision))
                     db.session.add(decision)
                     db.session.commit()
+
+
+@manager.command
+def export():
+    "Export data from db"
+    export_data = {'version': '1.0.0', }
+    export_data['registers'] = [str(r) for r in Register.query.all()]
+
+    cases = Case.query.all()
+    export_data['cases'] = []
+    for case in cases:
+        case.normalize()
+        export_data['cases'].append({
+            'id': str(case.id),
+            'register': str(case.register),
+            'book': str(case.book_id),
+            'description': case.description,
+        })
+
+    protocols = Protocol.query.all()
+    export_data['protocols'] = []
+    for protocol in protocols:
+        protocol.normalize()
+        export_data['protocols'].append({
+            'id': protocol.id,
+            'protocol_id': protocol.protocol_txt,
+            'case': protocol.case_id,
+            'date': protocol.protocol_date,
+            'description': protocol.description,
+        })
+
+    decisions = Decision.query.all()
+    export_data['decisions'] = []
+    for decision in decisions:
+        decision.normalize()
+        export_data['decisions'].append({
+            'id': decision.id,
+            'protocol': decision.protocol_id,
+            'decision_id': decision.decision_id,
+            'date': decision.decision_date,
+            'topic': decision.topic,
+            'description': decision.description,
+        })
+
+    resolutions = Resolution.query.all()
+    export_data['resolutions'] = []
+    for resolution in resolutions:
+        resolution.normalize()
+        export_data['resolutions'].append({
+            'id': resolution.id,
+            'case': resolution.case_id,
+            'decision': resolution.decision_id,
+            'resolution_id': resolution.resolution_id,
+            'date': resolution.resolution_date,
+            'description': resolution.description,
+        })
+    print(export_data)
+    new_export_data = {
+        'version': export_data['version'],
+        'cases': export_data['cases'],
+        'protocols': export_data['protocols'],
+        'decisions': export_data['decisions'],
+        'resolutions': export_data['resolutions'],
+    }
+    print(yaml.dump(export_data))
